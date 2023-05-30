9daff3487c6cf6bce96b970d7e52fdf545a2e363
Filip Tatíček <filtaticek@seznam.cz>
2023-05-30 21:33:28 +0200

    refactoring the code

--- app.js
+++ app.js
@@ -1,37 +1,5 @@
 const http = require("http")
-const fs = require("fs")
-
-function serverHandler(req, res) {
-  const { url, method } = req
-  if (url === "/") {
-    res.setHeader("Content-Type", "text/html")
-    res.write(`
-      <html>
-        <title>Write in something</title>
-        <form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>
-      </html>
-    `)
-    return res.end()
-  }
-
-  if (url === "/message" && req.method === "POST") {
-    const body = []
-    res.setHeader("Location", "/") //sending us back to home page
-    req.on("data", (chunk) => {
-      body.push(chunk)
-    })
-    req.on("end", () => {
-      const parseBody = Buffer.concat(body).toString()
-      const message = parseBody.split("=")[1]
-      console.log(message)
-      //with writeFileSYNC, the following code will stop until the file is created
-      fs.writeFile("message.txt", message, (err) => {
-        res.writeHead(302)
-        return res.end("Request succesfuly made...")
-      })
-    })
-  }
-}
+const serverHandler = require("./routes")
 
 const server = http.createServer(serverHandler)
 
