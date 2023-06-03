da939409825ee2b42d9f56673e589bca01e82374
Filip Tatíček <filtaticek@seznam.cz>
2023-06-03 16:13:08 +0200

    adding some handlers

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const express = require("express")
+const bodyParser = require("body-parser")
 
 const app = express()
+app.use(bodyParser.urlencoded()) //parser used for parsing the body of the form request. Req.body would be undefined without it
 
-app.use((req, res, next) => {
-  console.log("In the middleware! ")
-  next() //only if we call this next function, other middlewares may be run
+app.get("/api/send_post", (req, res, next) => {
+  // 1) we can make difference between app.get and app.post, app.use is for any request
+  // 2) the more specific middleware has to be first, otherwise it will never be reached
+  res.send(
+    '<form method="POST" action="/api/recieve_post"><input type="text" name="title"></input><button type="submit">submit</button></form>  '
+  )
 })
 
-app.use((req, res, next) => {
-  res.send("<p>Hello from Express </p>")
+app.use("/api/recieve_post", (req, res, next) => {
+  const body = req.body
+  res.redirect("/api/send_post")
+  console.log("Přišlo to super! ", body)
+})
+
+app.get("/", (req, res, next) => {
+  res.send(
+    "<form action='/api/send_post'><button type='submit'>Send post</button></form>"
+  )
 })
 
 app.listen(3000)
