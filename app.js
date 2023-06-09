d5990bcc1eb898fd8e49750e57999cd23e2c34d7
Filip Tatíček <filtaticek@seznam.cz>
2023-06-09 17:41:39 +0200

    First connecting to the MySQL database

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const bodyParser = require("body-parser")
 const adminRouter = require("./routes/admin")
 const productRouter = require("./routes/product")
 const controllers = require("./controllers/index")
+const db = require("./util/database")
 
 const app = express()
 app.use(bodyParser.urlencoded())
 //parser used for parsing the body of the form request. Req.body would be undefined without it
+
+db.execute("SELECT * FROM products")
+  .then((result) => console.log("Alles gut! ", result[0]))
+  .catch((err) => console.log("An error! ", err))
 
 app.use("/admin", adminRouter)
 app.use("/product", productRouter)
---
+++ util/database.js
@@ -0,0 +1,12 @@
+// by this file, we will connect to the database
+const mysql = require("mysql2")
+const env = require("dotenv").config()
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  database: "node-complete",
+  password: process.env.DATABASE_PASSWORD,
+})
+
+module.exports = pool.promise()
