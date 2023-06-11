472d91b0978c510ade10ed3e551760c59eb2a1a1
Filip Tatíček <filtaticek@seznam.cz>
2023-06-11 19:01:36 +0200

    adding cart and cartItem

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const controllers = require("./controllers/index")
 const sequelize = require("./util/database")
 const Product = require("./models/product")
 const User = require("./models/user")
+const Item = require("./models/cart")
+const CartItem = require("./models/cartItem")
+const Cart = require("./models/cart")
 
 const app = express()
 app.use(bodyParser.urlencoded()) //parser used for parsing the body of the req.body.
@@ -23,9 +26,13 @@ app.use(controllers.defaultPage) //default page
 
 Product.belongsTo(User, { constrains: true, onDelete: "CASCADE" }) //once  the user is deleted, so is the product
 User.hasMany(Product)
+User.hasOne(Cart)
+Cart.belongsTo(User)
+Cart.belongsToMany(Product, { through: CartItem })
+Product.belongsToMany(Cart, { through: CartItem })
 
 sequelize
-  .sync() //this function writes all of the sequelize models by default into the database
+  .sync({ force: true }) //this function writes all of the sequelize models by default into the database
   .then((result) => {
     return User.findByPk(1)
   })
